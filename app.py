1b157a0bc4b3118ceb5cc0de7457e6d31db279cd
samuel-velez <samuelvelez@outlook.com>
2021-03-03 09:48:48 -0800

    commented the .null in app.py and moved the csv to the main directory

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -22,14 +21,12 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 
-
-
 game = pd.read_csv("vgsales.csv")
 
 game.Year = game.Year.astype("Int64")
-print(nulls.groupby("Year").size())
-print(nulls.groupby("Genre").size())
-print(nulls.groupby("Platform").size())
+# print(nulls.groupby("Year").size())
+# print(nulls.groupby("Genre").size())
+# print(nulls.groupby("Platform").size())
 
 #For the analysis of sales - melting the NA,EU,JP,Other and Total columns
 game_melt = game.melt(id_vars=["Rank", "Name","Platform","Year","Genre","Publisher"], 
@@ -45,8 +42,6 @@ genre_count = alt.Chart(game).mark_bar().encode(
     alt.Y("count()",title="Number of games",type="quantitative"),
     alt.Color("count()",scale=alt.Scale(scheme='category20b'),legend=None),
     alt.Tooltip("count()"))
-
-
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
