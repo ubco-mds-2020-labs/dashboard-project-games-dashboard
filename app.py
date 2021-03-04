3ba8fb0915cefa511d177ac5fc0c257b12a8e9ed
Conrad-Yeung <conrad.yeung@hotmail.ca>
2021-03-04 12:14:39 -0500

    adding structure, base app and heroku

--- app.py
+++ app.py
@@ -1,65 +1,122 @@
-# -*- coding: utf-8 -*-
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
+#Dash components
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
-import pandas as pd
-import pandas as pd
-import numpy as np
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+import dash_table
+from dash.dependencies import Input, Output
+
+#Graphing components
 import altair as alt
-from altair_saver import save
-alt.renderers.enable('mimetype')
-alt.data_transformers.enable('data_server')
+import pandas as pd
+alt.data_transformers.disable_max_rows() #Disable max rows
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-
-
-
-game = pd.read_csv("vgsales.csv")
-
+#Read in data & basic wrangling 
+game = pd.read_csv("data/vgsales.csv")
 game.Year = game.Year.astype("Int64")
-print(nulls.groupby("Year").size())
-print(nulls.groupby("Genre").size())
-print(nulls.groupby("Platform").size())
+game_melt = game.melt(id_vars=["Rank", "Name","Platform","Year","Genre","Publisher"], var_name="Region", value_name="Sales").reset_index(drop=True)
+sales_data = game_melt.loc[game_melt.Region != "Global_Sales",:]
+sorted_genre_totalsales = list(game.groupby("Genre").sum().sort_values("Global_Sales",ascending=False).index)
 
-#For the analysis of sales - melting the NA,EU,JP,Other and Total columns
-game_melt = game.melt(id_vars=["Rank", "Name","Platform","Year","Genre","Publisher"], 
-        var_name="Region", 
-        value_name="Sales").reset_index(drop=True)
+#Initialize app
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
-sorted_genre_count = list(game.groupby("Genre").size().sort_values(ascending=False).index)
-sorted_year_count = list(game.groupby("Year").size().sort_values(ascending=False).index)
-sorted_platform_count = list(game.groupby("Platform").size().sort_values(ascending=False).index)
+#Side Bar
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+sidebar = html.Div(
+    [
+        html.H1("Game Protoype", className="display-4"),
+        html.Hr(),
+        html.P("Filters for graphs and tables", className="lead"),
+        html.P("Region Dropdown"),
+        dcc.Dropdown(
+            id="region_filter",
+            value='NA_Sales',  # REQUIRED to show the plot on the first page load
+            options=[{'label': regions, 'value': regions} for regions in list(sales_data.Region.unique())]
+            ),
+        html.P("Table size"),
+        dcc.RadioItems(
+            id="table_display",
+            options = [{"label":"5","value":5},
+                {"label":"10","value":10},
+                {"label":"15","value":15},
+                {"label":"20","value":20}],
+            value = 5,
+            labelStyle={'display': 'block'}
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
 
-genre_count = alt.Chart(game).mark_bar().encode(
-    alt.X("Genre",type="nominal",sort=sorted_genre_count),
-    alt.Y("count()",title="Number of games",type="quantitative"),
-    alt.Color("count()",scale=alt.Scale(scheme='category20b'),legend=None),
-    alt.Tooltip("count()"))
+#Main Body
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+content = dbc.Container([
+    dbc.Row([
+        dbc.Col(
+            html.Iframe(
+                id = "titles_graph",
+                style={'border-width': '1', 'width': '400px', 'height': '450px'}
+            )
+        ),
+        dbc.Col(
+            dash_table.DataTable(
+                id = "datatable",
+                columns = [{"name":i , "id":i} for i in sales_data.columns],
+                data = sales_data.loc[0:5,].to_dict("records")
+            )
+        )    
+    ])
+    ],
+    style = CONTENT_STYLE
+)
+
+#Putting together Sidebar + Main Body
+app.layout = dbc.Container([sidebar,content])
+
+#Call back for table
+@app.callback(
+    Output("datatable","data"), #Output to ID: datatable for value:data
+    Input("region_filter","value"),
+    Input("table_display","value"))
+def update_table(region_filter,n):
+    filtered_data = sales_data[sales_data.Region==region_filter].sort_values("Sales",ascending=False)[0:n]
+    return filtered_data.to_dict("records")
 
 
+#Call back for Graph 1: Region Sales + Title Name
+@app.callback(
+    Output("titles_graph","srcDoc"), #Output to ID: titles_graph for value:srcDoc
+    Input("region_filter","value"))
+def title_plot(region_filter):
+    sorted_genre = list(sales_data[sales_data.Region==region_filter].groupby("Genre").sum().sort_values("Sales",ascending=False).index)  
+    chart=alt.Chart(sales_data[sales_data.Region==region_filter]).mark_circle(size=50).encode(
+        alt.X("Genre",sort=sorted_genre,title=None),
+        alt.Y("Sales:Q",stack=None, title="Sales (in millions)"),
+        alt.Color("Genre",scale=alt.Scale(scheme='category20')),
+        alt.Tooltip("Name"))
+    chart = chart + alt.Chart(sales_data[sales_data.Region==region_filter].sort_values("Sales",ascending=False).iloc[:5,]).mark_text(align = "left", dx=10).encode(
+        alt.X("Genre",sort=sorted_genre),
+        alt.Y("Sales:Q"),
+        text="Name").properties(title=region_filter)
+    return chart.to_html()
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+#Call back for Table 
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=genre_count
-    )
-])
-
+#Convention
 if __name__ == '__main__':
     app.run_server(debug=True)
+
