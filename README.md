2a122e72a11ec6c5801cad6391624fb873bff900
samuel-velez <samuelvelez@outlook.com>
2021-03-10 14:21:16 -0800

    Tab version to main

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-This dashboard presents the composition of historical videogame sales from 1980 to 2020, according to their genre (Roleplaying, Strategy, Action, etc.), console (PlayStation, Xbox, PC, etc.) and region (Europe, Asia, North America, etc.). It includes bar charts, scatter plots and violing plots which are all color coded to better convey their message and compare to each other. They are all adjustable to the preferred point of view of the user, including a slider to adjust the visualized date range and dropdown lists to allow the user to filter for the region, platform and genre of interest. (i.e. Nintendo sales between 1990 and 2000 for action games) A list shows the top seller titles for the selected filters, with a summary of their information, and a selector allows the user to show more or less titles on this table.
+## Motivation
+
+Videogames are no longer an incipient industry but a well established field of entertainment, but its similarities and differences with other mediums makes them hard to understand people outside of it, including investors, media, and other laypersons. Understanding the underlying information on why the market evolves across time the way it does in different regions, platforms and genre facilitates understanding the mechanics and cultural differences that push this industry forward.
+
+This dashboard's purpose is to help the viewer understand the reality of the videogame market with as much detail as possible, down to the individual title sales numbers. It includes the composition of historical videogame sales from 1980 to 2020, according to their genre (Roleplaying, Strategy, Action, etc.), console (PlayStation, Xbox, PC, etc.) and region (Europe, Asia, North America, etc.).
+## Description
+
+The Games Dashboard includes bar charts, scatter plots and violin plots which are all color coded to better convey their message and compare to each other. They are all adjustable to the preferred point of view of the user, including a slider to adjust the visualized date range and dropdown lists to allow the user to filter for the region, platform and genre of interest. (i.e. Nintendo sales between 1990 and 2000 for action games) A list shows the top seller titles for the selected filters, with a summary of their information, and a selector allows the user to show more or less titles on this table.
+
+The Games Dashboard includes visualizations and widgets to help the user understand the industry.
+
+## The team
+
+We are students in the Master's of Data Science program at the University of British Columbia. Our motivation is to create a tool to help its users better understand the video game industry:
+* Aamir Khan (jadoon.engr@gmail.com)
+* Conrad Yeung (conrad.yeung@hotmail.ca)
+* Mandar Kathe
+* Samuel Velez (savelez@ubc.mail.ca)
+
+## Contributions
+We are interested in implementing live updates to our table connected to our source's API. If you can help us with this, or generally have any ideas on how we can improve this project, please contact any member of the team.
 
 ![Sketch](AppSketch.jpg)
--- src/app.py
+++ src/app.py
@@ -12,30 +12,145 @@ import pandas as pd
 alt.data_transformers.disable_max_rows() #Disable max rows
 
 #Read in data & basic wrangling 
-game = pd.read_csv("data/vgsales.csv")           # Modified (by Aamir)
-
+game = pd.read_csv("vgsales.csv")
 game.Year = game.Year.astype("Int64")
 game_melt = game.melt(id_vars=["Rank", "Name","Platform","Year","Genre","Publisher"], var_name="Region", value_name="Sales").reset_index(drop=True)
 sales_data = game_melt.loc[game_melt.Region != "Global_Sales",:]
 sorted_genre_totalsales = list(game.groupby("Genre").sum().sort_values("Global_Sales",ascending=False).index)
 
-#Global Variables (by Aamir)
+#Data wrangling 
+sales_data = game_melt.loc[game_melt.Region != "Global_Sales",:]
+sales_data_platform = sales_data.groupby(["Platform","Year","Genre","Region"]).sum().reset_index()[["Platform","Year","Genre","Region","Sales"]]
+sales_data_publisher = sales_data.groupby(["Publisher","Year","Genre","Region"]).sum().reset_index()[["Publisher","Year","Genre","Region","Sales"]]
+
+#Global variables
 regions = {"NA_Sales": "North America", "EU_Sales": "Europe", "JP_Sales": "Japan", "Other_Sales": "Other Regions"}
 
 #Initialize app
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
-#Side Bar
+###Styling
+#
 SIDEBAR_STYLE = {
     "position": "fixed",
     "top": 0,
     "left": 0,
     "bottom": 0,
     "width": "16rem",
+    "height": "100%",
+    "z-index": 1,
+    "overflow-x": "hidden",
+    "padding": "0.5rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "width":"100%",
     "padding": "2rem 1rem",
     "background-color": "#f8f9fa",
 }
+
+###Cards
+card_graph_tab1 = dbc.Card(
+    dbc.CardBody(
+        [
+            dbc.Row([html.H4("Number of copies released", className="card-title")]),
+            dbc.Row([html.P("Dessagregation per platform, publisher and genre",className="card-text")]),
+            dbc.Row([
+            html.Iframe(
+                id = "region_releases",
+                style={'border-width': '0', 'width': '450px', 'height': '450px'}
+            )
+            ])
+        ]
+    ),
+)
+
+card_graph_tab2 = dbc.Card(
+    dbc.CardBody(
+        [
+            dbc.Row([html.H4("Number of copies sold", className="card-title")]),
+            dbc.Row([html.P("Dessagregation per platform, publisher and genre",className="card-text")]),
+            dbc.Row([
+            html.Iframe(
+                id = "region_sales",
+                style={'border-width': '0', 'width': '450px', 'height': '450px'}
+            )
+            ])
+        ]
+    ),
+)
+
+card_graph_tab3 = dbc.Card(
+    dbc.CardBody(
+        [
+            dbc.Row([html.H4("Top Game titles, Platforms and Publishers across Genres", className="card-title")]),
+            dbc.Row([html.P("Video game titles with detailed information",className="card-text")]),
+            dbc.Row([
+            html.Iframe(
+                id = "title_perf_graph",
+                style={'border-width': '0', 'width': '450px', 'height': '450px'}
+            ),
+            html.Iframe(
+                id = "platform_perf_graph",
+                style={'border-width': '0', 'width': '450px', 'height': '450px'}
+            ),
+            html.Iframe(
+                id = "publisher_perf_graph",
+                style={'border-width': '0', 'width': '450px', 'height': '450px'}
+            )
+            ])
+        ]
+    ),
+)
+card_table = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H4("Table of the top Game Titles", className="card-title"),
+            html.P("Stuff below",className="card-text"),
+            dash_table.DataTable(
+                id = "datatable",
+                columns = [{"name": "Global Ranking" , "id":"Rank"},
+                           {"name": "Game Title" , "id": "Name"},
+                           {"name": "Platform" , "id":"Platform"},
+                           {"name": "Release Year" , "id": "Year"},
+                           {"name": "Genre" , "id": "Genre"},
+                           {"name": "Publisher" , "id": "Publisher"},
+                           {"name": "Sales (in millions)" , "id": "Sales"}],
+                data = sales_data.loc[0:5,].to_dict("records"),
+                css=[{'selector': 'table', 'rule': 'table-layout: fixed'}],
+                style_cell={
+                    "whiteSpace":"normal",
+                    "height":"auto",
+                    "width": '{}%'.format(len(["Rank","Name","Year","Genre","Platform","Publisher","Sales"]))
+                },
+                style_cell_conditional=[
+                    {
+                        'if': {'column_id': c},
+                        'textAlign': 'left'
+                    } for c in ["Rank","Name","Year","Genre","Platform","Publisher"]
+                ],
+                style_data_conditional=[
+                    {
+                        'if': {'row_index': 'odd'},
+                        'backgroundColor': 'rgb(248, 248, 248)'
+                    }
+                ],
+                style_header={
+                    'backgroundColor': 'rgb(230, 230, 230)',
+                    'fontWeight': 'bold'
+                }
+            )
+        ]
+    ),
+)
+
+###Formatting page
+#
+#Sidebar 
 sidebar = html.Div(
     [
         html.H1("Game Protoype", className="display-4"),
@@ -45,7 +160,10 @@ sidebar = html.Div(
         dcc.Dropdown(
             id="region_filter",
             value='NA_Sales',  # REQUIRED to show the plot on the first page load
-            options=[{'label': regions, 'value': regions} for regions in list(sales_data.Region.unique())]
+            options=[{'label': "North America", 'value': "NA_Sales"},
+                     {'label': "Europe", 'value': "EU_Sales"},
+                     {'label': "Japan", 'value': "JP_Sales"},
+                     {'label': "Other", 'value': "Other_Sales"},]
             ),
         html.P("Table size"),
         dcc.RadioItems(
@@ -60,49 +178,68 @@ sidebar = html.Div(
     ],
     style=SIDEBAR_STYLE,
 )
-
-#Main Body
-CONTENT_STYLE = {
-    "margin-left": "18rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",
+#Tab Styles
+tabs_styles = {
+    'height': '44px',
 }
-content = dbc.Container([
-    dbc.Row([
-        dbc.Col(
-            html.Iframe(
-                id = "titles_graph",
-                style={'border-width': '1', 'width': '400px', 'height': '450px'}
-            )
-        ),
-        dbc.Col(
-            dash_table.DataTable(
-                id = "datatable",
-                columns = [{"name":i , "id":i} for i in sales_data.columns],
-                data = sales_data.loc[0:5,].to_dict("records")
-            )
-        )    
-    ]),
-    dbc.Row([   # Middle graphs (by Aamir)
-        dbc.Col(
-            html.Iframe(
-                id = "releases_graph",
-                style={'border-width': '1', 'width': '450px', 'height': '500px'}
-            )
-        ),
-        dbc.Col(
-            html.Iframe(
-                id = "sales_graph",
-                style={'border-width': '1', 'width': '450px', 'height': '500px'}
-            )
-        )    
-    ])
+tab_style = {
+    'borderBottom': '1px solid #d6d6d6',
+    'padding': '6px',
+    'fontWeight': 'bold'
+}
+tab_selected_style = {
+    'borderTop': '1px solid #d6d6d6',
+    'borderBottom': '1px solid #d6d6d6',
+    'backgroundColor': '#119DFF',
+    'color': 'white',
+    'padding': '6px'
+}
+#Main Body
+content = html.Div([
+    html.H3("Change the metrics:"),
+    dcc.Tabs(id='tabs', value='tab-1', children=[
+        dcc.Tab(label='Number of games released', value='tab-1', style = tab_style, selected_style=tab_selected_style),
+        dcc.Tab(label='Number of copies sold', value='tab-2', style = tab_style, selected_style=tab_selected_style),
+        dcc.Tab(label='Top titles', value='tab-3', style = tab_style, selected_style=tab_selected_style),
+    ],style=tabs_styles), 
+    html.Div(id='tabs-content')
     ],
-    style = CONTENT_STYLE
+    style=CONTENT_STYLE
 )
-
 #Putting together Sidebar + Main Body
-app.layout = html.Div([sidebar,content])#dbc.Container([sidebar,content])
+app.layout = html.Div([
+        sidebar,
+        content 
+])
+
+###Callbacks 
+#
+#Call back for Tabs
+@app.callback(Output('tabs-content', 'children'),
+              Input('tabs', 'value'))
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+                    dbc.Container([
+                    dbc.Row([card_graph_tab1])
+                    ])
+        ])
+    elif tab == 'tab-2':
+        return html.Div([
+                    dbc.Container([
+                    dbc.Row([card_graph_tab2])
+                    ])
+        ])
+    elif tab == 'tab-3':
+        return html.Div([
+                    dbc.Container([
+                        html.Br(),
+                        dbc.Row([card_graph_tab3]),
+                        html.Br(),
+                        html.Br(),
+                        dbc.Row([card_table])
+                    ])
+            ])
 
 #Call back for table
 @app.callback(
@@ -113,30 +250,9 @@ def update_table(region_filter,n):
     filtered_data = sales_data[sales_data.Region==region_filter].sort_values("Sales",ascending=False)[0:n]
     return filtered_data.to_dict("records")
 
-
-#Call back for Graph 1: Region Sales + Title Name
+#Call back for Tab 1 - Graph 1: Region sales
 @app.callback(
-    Output("titles_graph","srcDoc"), #Output to ID: titles_graph for value:srcDoc
-    Input("region_filter","value"))
-def title_plot(region_filter):
-    sorted_genre = list(sales_data[sales_data.Region==region_filter].groupby("Genre").sum().sort_values("Sales",ascending=False).index)  
-    chart=alt.Chart(sales_data[sales_data.Region==region_filter]).mark_circle(size=50).encode(
-        alt.X("Genre",sort=sorted_genre,title=None),
-        alt.Y("Sales:Q",stack=None, title="Sales (in millions)"),
-        alt.Color("Genre",scale=alt.Scale(scheme='category20')),
-        alt.Tooltip("Name"))
-    chart = chart + alt.Chart(sales_data[sales_data.Region==region_filter].sort_values("Sales",ascending=False).iloc[:5,]).mark_text(align = "left", dx=10).encode(
-        alt.X("Genre",sort=sorted_genre),
-        alt.Y("Sales:Q"),
-        text="Name").properties(title=region_filter)
-    return chart.to_html()
-
-
-####################################################################################################
-# Aamir Code
-#Call back for Graph Middle 1: Region Releases + Title Name
-@app.callback(
-    Output("releases_graph","srcDoc"), #Output to ID: titles_graph for value:srcDoc
+    Output("region_sales","srcDoc"), #Output to ID: titles_graph for value:srcDoc
     Input("region_filter","value"))
 def title_plot(region_filter):
     #2) Lets look at sales across Genres for each Region
@@ -160,14 +276,11 @@ def title_plot(region_filter):
                     titleFontSize=13).configure_title(fontSize = 25,subtitleFontSize=15) 
     return genre_plots.to_html()
 
-
-
-
-#Call back for Graph Middle 1: Region Releases + Title Name
+#Call back for Tab 2 - Graph 1: Region releases
 @app.callback(
-    Output("sales_graph","srcDoc"), #Output to ID: titles_graph for value:srcDoc
+    Output("region_releases","srcDoc"), #Output to ID: titles_graph for value:srcDoc
     Input("region_filter","value"))
-def title_plot(region_filter):
+def title_plot(region_filter,max_year=2020):
     #1) Basic Exploratory visualisations of things we noted in the Initial Thoughts
     #Counts of number of games in each genre, platform and number of games released in each year
     #Genre and Platform counts are coloured by number of counts and sorted from largest to smallest
@@ -184,10 +297,63 @@ def title_plot(region_filter):
     }).configure_title(fontSize = 25,subtitleFontSize=15)
     return genre_count.to_html()
 
-####################################################################################################
 
+#Call back for Tab 3 - Graph 1: Title performance
+@app.callback(
+    Output("title_perf_graph","srcDoc"), #Output to ID: titles_graph for value:srcDoc
+    Input("region_filter","value"))
+def title_plot(region_filter,max_year=2020):
+    sorted_genre = list(sales_data[sales_data.Region==region_filter].groupby("Genre").sum().sort_values("Sales",ascending=False).index)
+    filtered_set = sales_data[sales_data.Year <= max_year].groupby(["Name","Genre","Region"]).sum().reset_index()[["Name","Genre","Region","Sales"]]
+    chart=alt.Chart(filtered_set[filtered_set.Region==region_filter]).mark_circle(size=50).encode(
+        alt.X("Genre",sort=sorted_genre,title=None),
+        alt.Y("Sales:Q",stack=None, title="Sales (in millions)"),
+        alt.Color("Genre",scale=alt.Scale(scheme='category20'),legend=None),
+        alt.Tooltip("Name"))
+    chart = chart + alt.Chart(filtered_set[filtered_set.Region==region_filter].sort_values("Sales",ascending=False).iloc[:5,]).mark_text(align = "left", dx=10).encode(
+        alt.X("Genre",sort=sorted_genre),
+        alt.Y("Sales:Q"),
+        text="Name").properties(title = "By Game Title")
+    return chart.to_html()
+
+#Call back for Tab 3 - Graph 2: Platform performance 
+@app.callback(
+    Output("platform_perf_graph","srcDoc"), #Output to ID: titles_graph for value:srcDoc
+    Input("region_filter","value"))
+def platform_plot(region_filter,max_year=2020):
+    sorted_genre = list(sales_data[(sales_data.Region==region_filter) & (sales_data.Year <= max_year)].groupby("Genre").sum().sort_values("Sales",ascending=False).index)   
+    filtered_set = sales_data_platform[sales_data_platform.Year <= max_year].groupby(["Platform","Genre","Region"]).sum().reset_index()[["Platform","Genre","Region","Sales"]]
+
+    chart=alt.Chart(filtered_set[filtered_set.Region==region_filter]).mark_circle(size=50).encode(
+        alt.X("Genre",sort=sorted_genre,title=None),
+        alt.Y("Sales:Q",stack=None, title="Sales (in millions)"),
+        alt.Color("Genre",scale=alt.Scale(scheme='category20'),legend=None),
+        alt.Tooltip("Platform"))
+    chart = chart + alt.Chart(filtered_set[filtered_set.Region==region_filter].sort_values("Sales",ascending=False).iloc[:5,]).mark_text(align = "left", dx=10).encode(
+        alt.X("Genre",sort=sorted_genre),
+        alt.Y("Sales:Q"),
+        text="Platform").properties(title = "By Platform")
+    return chart.to_html()
+
+#Call back for Tab 3 - Graph 3: Publisher performance
+@app.callback(
+    Output("publisher_perf_graph","srcDoc"), #Output to ID: titles_graph for value:srcDoc
+    Input("region_filter","value"))
+def publisher_plot(region_filter,max_year=2020):
+    sorted_genre = list(sales_data[(sales_data.Region==region_filter) & (sales_data.Year <= max_year)].groupby("Genre").sum().sort_values("Sales",ascending=False).index)   
+    filtered_set = sales_data_publisher[sales_data_publisher.Year <= max_year].groupby(["Publisher","Genre","Region"]).sum().reset_index()[["Publisher","Genre","Region","Sales"]]
+
+    chart=alt.Chart(filtered_set[filtered_set.Region==region_filter]).mark_circle(size=50).encode(
+        alt.X("Genre",sort=sorted_genre,title=None),
+        alt.Y("Sales:Q",stack=None, title="Sales (in millions)"),
+        alt.Color("Genre",scale=alt.Scale(scheme='category20')),
+        alt.Tooltip("Publisher"))
+    chart = chart + alt.Chart(filtered_set[filtered_set.Region==region_filter].sort_values("Sales",ascending=False).iloc[:5,]).mark_text(align = "left", dx=10).encode(
+        alt.X("Genre",sort=sorted_genre),
+        alt.Y("Sales:Q"),
+        text="Publisher").properties(title = "By Publisher")
+    return chart.to_html()
 
 #Convention
 if __name__ == '__main__':
-    app.run_server(debug=True)
-
+    app.run_server(debug=True, port = 8051)
